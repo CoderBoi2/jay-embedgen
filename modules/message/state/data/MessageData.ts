c5292e80f6bbd4e8cf53014310536174905a9ec5
CoderBoi2 <103612036+CoderBoi2@users.noreply.github.com>
2022-11-04 05:44:38 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ modules/message/state/data/MessageData.ts
@@ -0,0 +1,10 @@
+import type { EmbedData } from "./EmbedData"
+
+export type MessageData = {
+  readonly content?: string | null
+  readonly embeds?: readonly EmbedData[] | null
+  readonly username?: string
+  readonly avatar_url?: string
+  readonly files?: readonly File[]
+  readonly attachments?: readonly unknown[]
+}
